91aeb14e007d3727652a25ea3c2a15462700df81
yamaguchi shota <ayshot0918@gmail.com>
2022-08-10 11:02:51 +0900

    [add] readme.md saveAssetImage.py

---
+++ saveAssetImage.py
@@ -0,0 +1,112 @@
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+import os
+import time
+import shutil
+import logging
+import time
+import logging
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+logging.basicConfig(
+    filename = '.\\saveAssetImage.log',
+    level = logging.DEBUG, 
+    format="%(asctime)s: %(levelname)s %(message)s"
+)
+
+# サービス
+class MySvc (win32serviceutil.ServiceFramework):
+    _svc_name_ = "saveAssetImage"
+    _svc_display_name_ = "壁紙保管サービス"
+    _svc_description_='ロック画面の画像を保管する'
+
+    # 初期化
+    def __init__(self,args):
+        win32serviceutil.ServiceFramework.__init__(self,args)
+        self.stop_event = win32event.CreateEvent(None,0,0,None)
+        socket.setdefaulttimeout(60)
+        self.stop_requested = False
+        self.args = args
+
+    # サービス停止
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.stop_event)
+        logging.info('停止リクエストを受け付けました')
+        self.stop_requested = True
+
+    # サービス開始
+    def SvcDoRun(self):
+        servicemanager.LogMsg(
+            servicemanager.EVENTLOG_INFORMATION_TYPE,
+            servicemanager.PYS_SERVICE_STARTED,
+            (self._svc_name_,'')
+        )
+
+        self.main_loop()
+
+    def main_loop(self):
+        logging.info('サービスを開始しました')
+
+        if not len(self.args) == 3 :
+            logging.error('引数エラー サービスを停止します')
+            return
+        
+        logging.info('コピー元：%s' % self.args[1])
+        logging.info('コピー先：%s' % self.args[2])
+        event_handler = ChangeHandler(self.args[2])
+        observer = Observer()
+        observer.schedule(event_handler, self.args[1], recursive=True)
+        observer.start()
+
+        while True:
+            if self.stop_requested:
+                logging.info('サービス停止中...')
+                observer.stop()
+                break
+
+            time.sleep(0.1)
+        
+        observer.join()
+        logging.info("サービスを停止しました")
+        return
+
+# フォルダ監視
+class ChangeHandler(FileSystemEventHandler):
+
+    def __init__(self, target_dir):
+        self.target_dir = target_dir
+
+    # ファイル作成時、保管先にコピーする
+    def on_created(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        logging.info('%sが作成されました' % filename)
+        while not os.access(filepath,os.W_OK):
+            time.sleep(0.5)
+        try:
+            time.sleep(0.5)
+            shutil.copy(filepath, os.path.join(self.target_dir, filename + ".jpg"))
+            logging.info('%sをコピーしました' % filename)
+        except Exception as e:
+            logging.error(e)
+
+    # ファイル変更時 ログ出力のみ
+    def on_modified(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        logging.info('%sが変更されました' % filename)
+
+    # ファイル削除時 ログ出力のみ
+    def on_deleted(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        logging.info('%sが削除されました' % filename)
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(MySvc)
